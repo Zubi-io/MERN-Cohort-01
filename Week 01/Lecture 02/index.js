b5c33a3469d8e369bc57792072522b6b1154ff50
Debchakra Majumder <dmajumder4292@gmail.com>
2020-07-29 19:20:09 +0530

    Lecture 02

---
+++ Week 01/Lecture 02/index.js
@@ -0,0 +1,180 @@
+// alert("Hi! My name is JS")
+// document.getElementById("demo").innerHTML = "Text for Demo!"
+// var a = 15;
+
+// if(a % 5 === 0){
+//     console.log("Divisable by 5")
+// } else if (a % 3 === 0) {
+//     console.log("Divisable by 3")
+// } else if (a % 2 === 0) {
+//     console.log("Divisable by 2")
+// } else {
+//     console.log("Not Divisable by 2, 3 0r 5")
+// }
+
+// console.log('2' === 2)
+
+// var day;
+
+// switch(new Date().getDay() - 1) {
+//     case 0:
+//         day = "Sunday"
+//         break;
+//     case 1:
+//         day = "Monday"
+//         break;
+//     case 2:
+//         day = "Tuesday"
+//         break;
+//     case 3:
+//         day = "Wednesday"
+//         break;
+//     case 4:
+//         day = "Thursday"
+//         break;
+// }
+
+// console.log(day)
+
+// var i = 5;
+
+// while(i >= 0){
+//     console.log(i)
+//     i--
+// }
+
+// for(var i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// for(var i = 0; i <= 100; i++){
+//     if(i%8 === 0){
+//         console.log(i)
+//     }
+// }
+
+// function cube (number){
+//     console.log("calling!!!!")
+//     var i = number * number * number;
+//     return i;
+// }
+
+// var cube = function (number){
+//     console.log("calling!!!!")
+//     var i = number * number * number;
+//     return i;
+// }
+
+// var cube = (number) => {
+//     console.log("calling!!!!")
+//     var i = number * number * number;
+//     return i;
+// }
+
+// var cube = (number) => { return number * number * number }
+
+// console.log(cube(3))
+// var cubeof27 = cube(27)
+// var cubeof100 = cube(100)
+
+// console.log(cubeof27, cubeof100)
+
+// var arr = [1,2,3,4,5,6,7,8,9]
+
+// console.log(arr[4])
+
+// var newArr = [3,4,5,6]
+
+// arr.push(10)
+
+// arr.push(11)
+
+// arr.pop()
+
+// arr.pop()
+
+// arr.shift()
+
+// arr.shift()
+
+// arr.unshift(0)
+
+// arr.unshift([1,2,3])
+
+// console.log(arr.indexOf('zubi'));
+
+// console.log(arr.slice(2,6))
+
+// console.log(arr);
+
+var a = 55
+
+var obj = {
+    language: "JS",
+    petName: "Emma",
+    age: a,
+    nesObj: {
+        item: "test",
+        item2: "test2"
+    },
+    hobbies: ["football", "music", "formula1"],
+    name: "Deb"
+}
+
+console.log(obj.age)
+
+// console.log(obj.name);
+
+// console.log(Object.values(obj))
+
+// var obj = {
+//     data: [1,2,3,4,5,6],
+//     name: "Deb",
+//     print: function () {
+//         for(var i = 0; i < this.data.length; i++){
+//             console.log(this.name)
+//             console.log(this.data[i])
+//         }
+//     }
+// }
+
+// function print(data){
+//     for(var i = 0; i < data.length; i++){
+//         console.log(data[i])
+//     }
+// }
+
+// obj.print()
+
+// var a = 10;
+
+// function test () {
+//     var b = 20;
+//     function test2 () {
+//         var c = 30
+//         console.log(b)
+//     }
+//     test2()
+//     console.log(c);
+//     console.log(b)
+//     console.log(a);
+// }
+
+// test()
+
+// console.log(b)
+
+// console.log(val)
+
+// let val = 20;
+
+// val = 50
+
+// console.log(val)
+
+// const val = 10;
+
+// val = 30;
+
+// console.log(val)
+
